8fa0fdd751b22ef2f8e9a7ab441774fcc281c956
George1BM19CS197 <71543064+George1BM19CS197@users.noreply.github.com>
2021-12-07 09:43:38 +0530

    Update 2-3treeInsertOperation.c

--- LAB 5/2-3treeInsertOperation.c
+++ LAB 5/2-3treeInsertOperation.c
@@ -1,22 +1,22 @@
-void insert(node *candidate, t *key) { //Here key is the element to be inserted in the candidate
-if(candidate == nullptr) {
-root = new node(key);
-return;
-}
-if(candidate-&gt;isleaf()) {
-candidate-&gt;store(key);
-}else {
-if(*candidate-&gt;firstkey &gt; *key) {
-insert(candidate-&gt;less, key); //Insert to left subtree.
-}else if(*candidate-&gt;firstkey &lt;= *key &amp;&amp; candidate-&gt;is2node()) {
-insert(candidate-&gt;btwn, key); //Insert to mid subtree.
-}else {
-if(*candidate-&gt;secondkey &gt; *key) {
-insert(candidate-&gt;btwn, key); //Insert to mid subtree.
-}else {
-insert(candidate-&gt;great, key); //Insert to right subtree.
-}
-}
-}
-split(candidate); //This function is to balance the tree incase of an overflow.
+void insert(node *candidate, t *key) 
+	{if(candidate == nullptr) {
+		root = new node(key);
+		return;
+	}
+	if(candidate->isleaf()) {
+		candidate->store(key);
+	}else {
+		if(*candidate->firstkey > *key) {
+			insert(candidate->less, key);
+		}else if(*candidate->firstkey <= *key && candidate->is2node()) {
+			insert(candidate->btwn, key); 
+		}else {
+			if(*candidate->secondkey > *key) {
+				insert(candidate->btwn, key); 
+			}else {
+				insert(candidate->great, key); 
+			}
+		}
+	}
+	split(candidate); 	
 }
